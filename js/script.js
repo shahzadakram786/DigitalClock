83e1cc9f338f58a791ee03e93fcd5fb0d14178dc
shahzadakram786 <alyakram786@gmail.com>
2024-08-30 18:55:21 +0500

    .

--- js/script.js
+++ js/script.js
@@ -1,25 +1,25 @@
-function updateCLock() {
-    let now = new Date();
-    let hours = now.getHours();
-    let minutes = now.getMinutes();
-    let seconds = now.getSeconds();
+// function updateCLock() {
+//     let now = new Date();
+//     let hours = now.getHours();
+//     let minutes = now.getMinutes();
+//     let seconds = now.getSeconds();
 
-    let timePeriod = hours >= 12 ? "PM" : "AM";
-    hours = hours % 12;
-    hours = hours ? hours : 12;
+//     let timePeriod = hours >= 12 ? "PM" : "AM";
+//     hours = hours % 12;
+//     hours = hours ? hours : 12;
 
-    hours = hours < 10 ? "0" + hours : hours;
-    minutes = minutes < 10 ? "0" + minutes : minutes;
-    seconds = seconds < 10 ? "0" + seconds : seconds;
+//     hours = hours < 10 ? "0" + hours : hours;
+//     minutes = minutes < 10 ? "0" + minutes : minutes;
+//     seconds = seconds < 10 ? "0" + seconds : seconds;
 
-    const hoursElement = document.getElementById("hours");
-    const minutesElement = document.getElementById("minutes");
-    const secondsElement = document.getElementById("seconds");
-    const timePeriodElement = document.getElementById("period");
+//     const hoursElement = document.getElementById("hours");
+//     const minutesElement = document.getElementById("minutes");
+//     const secondsElement = document.getElementById("seconds");
+//     const timePeriodElement = document.getElementById("period");
 
-    hoursElement.textContent = hours;
-    minutesElement.textContent = minutes;
-    secondsElement.textContent = seconds;
-    timePeriodElement.textContent = timePeriod;
-}
-setInterval(updateCLock, 1000);
+//     hoursElement.textContent = hours;
+//     minutesElement.textContent = minutes;
+//     secondsElement.textContent = seconds;
+//     timePeriodElement.textContent = timePeriod;
+// }
+// setInterval(updateCLock, 1000);
